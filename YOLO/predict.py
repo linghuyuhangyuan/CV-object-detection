3c9fc402d033af91da98172b73393f0846a3ee3c
kay-ck <2926944247@qq.com>
2023-11-08 08:44:55 +0800

    update

--- YOLO/predict.py
+++ YOLO/predict.py
@@ -35,8 +35,8 @@ def decoder(pred):
     pred = pred.data  # torch.Size([1, 14, 14, 30])
     pred = pred.squeeze(0)  # torch.Size([14, 14, 30])
     # [中心坐标,长宽,置信度,中心坐标,长宽,置信度, 20个类别] x 7x7
-    contain1 = # 从pred中取出bbox1的置信度
-    contain2 = # 从pred中取出bbox2的置信度
+    # contain1 = # 从pred中取出bbox1的置信度
+    # contain2 = # 从pred中取出bbox2的置信度
     contain = torch.cat((contain1, contain2), 2) # torch.Size([14, 14, 2])
 
     mask1 = contain > 0.1 # 大于阈值, torch.Size([14, 14, 2]) content: tensor([False, False])
@@ -48,15 +48,15 @@ def decoder(pred):
         for j in range(grid_num):
             for b in range(2):
                 if mask[i, j, b] == 1:
-                    box = # 从pred中取出bbox的中心坐标及宽高
-                    contain_prob = # 从pred中取出bbox的置信度
+                    # box = # 从pred中取出bbox的中心坐标及宽高
+                    # contain_prob = # 从pred中取出bbox的置信度
                     xy = torch.FloatTensor([j, i]) * cell_size # cell左上角 up left of cell
                     box[:2] = box[:2] * cell_size + xy # return cxcy relative to image
                     box_xy = torch.FloatTensor(box.size()) # 转换成xy形式 convert[cx, cy, w, h] to [x1, y1, x2, y2]
                     # box[:2]是bbox的中心坐标，box[2:]是bbox的宽高
-                    box_xy[:2] = # 计算出bbox左上角的坐标
-                    box_xy[2:] = # 计算出bbox右下角的坐标
-                    max_prob, cls_index = # 从pred中取出20个类别的概率，并得到最大值及其索引
+                    # box_xy[:2] = # 计算出bbox左上角的坐标
+                    # box_xy[2:] = # 计算出bbox右下角的坐标
+                    # max_prob, cls_index = # 从pred中取出20个类别的概率，并得到最大值及其索引
                     if float((contain_prob * max_prob)[0]) > 0.1:
                         boxes.append(box_xy.view(1, 4))
                         cls_indexs.append(cls_index.item())
@@ -94,7 +94,7 @@ def nms(bboxes, scores, threshold=0.5):
         keep.append(i)
 
         # 计算box[i]与其余各框box[order[1:]]的IOU
-        ovr = 
+        # ovr = 
         ids = (ovr <= threshold).nonzero(as_tuple=False).squeeze() # 注意此时idx为[N - 1,], 而order为[N, ]
         if ids.numel() == 0:
             break
